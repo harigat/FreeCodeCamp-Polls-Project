e17b229012c014ffad7daffbb3a0319ccedacd26
Harshit <harsh35plus@gmail.com>
2016-06-28 14:05:44 +0530

    Styled create.html

--- polls/static/js/create.js
+++ polls/static/js/create.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('#add_form').on('click',function() {
     var formm= $("#empty_form").html().replace(/__prefix__/g, form_count);
     $(formm).insertBefore('#empty_form');
+	setTimeout(function(){$('.new:not(#empty_form>.new)').removeClass('new')},100);
     form_count++;
     $('#id_choicef-TOTAL_FORMS').val(form_count);});
 	}
