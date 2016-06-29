00599cbcc4bc7e7ebf2dac584257890189062c1e
Harshit <harsh35plus@gmail.com>
2016-06-29 15:13:58 +0530

    Styled login and register page, added slideDown jQuery animation on create.js, added titles

--- polls/static/js/create.js
+++ polls/static/js/create.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 	var form_count = $('#id_choicef-TOTAL_FORMS').val();
 	$('#add_form').on('click',function() {
     var formm= $("#empty_form").html().replace(/__prefix__/g, form_count);
-    $(formm).insertBefore('#empty_form');
-	setTimeout(function(){$('.new:not(#empty_form>.new)').removeClass('new')},100);
+    $(formm).css('display','none').insertBefore('#empty_form').slideDown(300);
     form_count++;
     $('#id_choicef-TOTAL_FORMS').val(form_count);});
 	}
